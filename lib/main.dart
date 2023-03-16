402ace8ad75dd716e385147ee42e097560969213
vishal kumar mauray <vishalmauray18@gmail.com>
2023-03-16 20:30:10 +0530

    update the UI

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider<TodoCubit>(
       create: (context) => TodoCubit(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'To Do',
         theme: ThemeData(
           primarySwatch: Colors.blue,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,58 +21,112 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     TextEditingController textInput = TextEditingController();
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text("TO DO's"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: textInput,
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          color: Colors.grey[400],
+          padding: const EdgeInsets.fromLTRB(32, 48, 32, 4),
+          child: Column(
+            children: [
+              Text(
+                'TO DO App',
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Type Something Here...',
+                            hintStyle: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          controller: textInput,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    reference.add(textInput.text);
-                  },
-                  icon: const Icon(Icons.add),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            BlocBuilder<TodoCubit, TodoState>(
-              builder: (context, state) {
-                return Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: state.tasks.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.all(8),
-                        color: Colors.amber,
-                        child: Row(
-                          children: [
-                            Text(state.tasks[index]),
-                            IconButton(
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.black,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        reference.add(textInput.text);
+                      },
+                      icon: const Icon(
+                        Icons.add_rounded,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              BlocBuilder<TodoCubit, TodoState>(
+                builder: (context, state) {
+                  return Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: state.tasks.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 70,
+                          margin: const EdgeInsets.only(bottom: 16),
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.check_circle_outline_rounded,
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  state.tasks[index],
+                                  style: Theme.of(context).textTheme.bodyLarge,
+                                ),
+                              ),
+                              IconButton(
                                 onPressed: () {
                                   reference.remove(index);
                                 },
-                                icon: const Icon(Icons.delete))
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                );
-              },
-            )
-          ],
+                                icon: const Icon(Icons.delete_outline),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
